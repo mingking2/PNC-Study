d0debc27317d3fa7818c307bc10ef4789f35385a
JeongYuJin <wjddbwls1230@naver.com>
2023-11-16 17:19:24 +0900

    feat 유진 과제 2

--- 유진_과제2.cpp
+++ 유진_과제2.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main() {
   int number;
-  int list[100] = {0};
-  int array[100][2];
+  int list[100] = {0}; //소수 넣어둘 배열
+  int array[100][2]; //합한 거 넣어둘 배열
   while(1){
     cout << "4 이상의 짝수를 입력하세요(0을 입력하면 종료): ";
     cin >> number;
-    if(number == 0) break;
-    else if(number % 2 != 0) continue;
-    else if(number <4) continue;
-    else {
+    if(number == 0) break; //0일 땐 멈추기
+    else if(number % 2 != 0) continue; //홀수면 다시
+    else if(number <4) continue; //4 이하면 다시
+    else { //셋 다 아닐 땐 실행
       int n = 0;
       for (int i = 2; i <= number; i++) {
         int num=0;
@@ -20,14 +20,14 @@ int main() {
         }
         if(num == 0) {list[n] = i; n++;} //소수 넣어둠 7 -> 2, 3, 5, 7
       }
-      /*for(int i = 0; list[i] != 0; i++){
+      /*for(int i = 0; list[i] != 0; i++){ //확인차 출력
         cout << list[i] << endl;
       }*/
       
       int m = 0;
       for(int i = 0; list[i] < number && list[i] > 0; i++){
         for(int j =0; list[j] < number && list[j] > 0; j++){
-          if((list[i] + list[j]) == number) {
+          if((list[i] + list[j]) == number) { //list 배열 안에 있는 소수 하나씩 더해보면서 number랑 같은거는 array 배열에 넣어두기
             array[m][0] = list[i];
             array[m][1] = list[j];
             m++;
@@ -35,14 +35,14 @@ int main() {
         }
       }
 
-     /* for(int i = 0; array[i][0] < number && array[i][0] > 0; i++){
+     /* for(int i = 0; array[i][0] < number && array[i][0] > 0; i++){ //확인차 출력
           cout<<array[i][0] << " "<< array[i][1] <<endl;
       }*/
       
-      int max = array[0][1] - array[0][0];
+      int max = array[0][1] - array[0][0]; //소수 중 차이가 제일 큰 두 수를 찾기 위해
       int a = array[0][0];
       int b = array[0][1];
-      for(int i = 1; i< 100; i++,m++){
+      for(int i = 1; array[i][0] < number && array[i][0] > 0 ; i++,m++){
         if(array[i][1] - array[i][0] > max) array[i][1] = a; array[i][0] = b;
       }
       cout << number << " = " << a << " + " << b << endl;

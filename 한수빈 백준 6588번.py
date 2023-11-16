af7d2766bb016af3fe01c54405d3d3546567e1ed
HanSuBin <han04245@naver.com>
2023-11-16 18:11:09 +0900

    한수빈 백준 제출

---
+++ 한수빈 백준 6588번.py
@@ -0,0 +1,21 @@
+a=[]
+for c in range(2,1000001):
+    if c==2:
+        a.append(2)
+    for d in range(1,c-1):
+        if c % d == 0:
+            break
+    else:
+        a.append(c) #리스트 1000,000개 만들었음
+p=0
+q=len[a]
+while True:
+    n=int(input())
+    for f in range(len(a/2)):
+        if n == a[p] + a[q]:
+            print(n, "=", a[p], "+", a[q] )
+            break
+        p=p+1
+        q=q-1
+    if n==0:
+        break

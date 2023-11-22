79587135e6449f8bee8892c3c6f1218b77b40f1d
DongChan Kim <dongkdc@naver.com>
2023-11-22 15:28:33 +0900

    조건문 수정

--- 김동찬 - 백준 1929.c
+++ 김동찬 - 백준 1929.c
@@ -2,16 +2,13 @@
 int main(void){
   int n, m;
 	scanf_s("%d %d", &n, &m);
-	for (n; n <= m; n++)
+	for (n; n <= m && n % 2 != 0 && n!=1 || n==2; n++)
 	{
-		if (n % 2 != 0 && n!=1 || n==2) // 2를 제외한 2의 배수와 1은 소수가 아님
-		{
 			int i = 2;
 			while (i <= n) {
 				if (i == n) printf("%d\n", n);
 				if (n % i == 0) break;
 				i++;
 			}
-		}
 	}
 }
